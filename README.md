1b396eb5aa9d03cbce55a30eff285cdfbc3c1fce
Aseem Savio <aseemsavio3@gmail.com>
2019-04-16 10:40:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# invenio-service-discovery
---
+++ src/main/java/com/savio/invenio/InvenioApplication.java
@@ -0,0 +1,13 @@
+package com.savio.invenio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvenioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvenioApplication.class, args);
+    }
+
+}
